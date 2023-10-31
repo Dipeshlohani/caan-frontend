1a8785bac25cef85dd60563e7eb9977ca0f3a244
Ga-urab <128809798+Ga-urab@users.noreply.github.com>
2023-10-31 16:35:37 +0545

    mui integrated, using backend provided in github

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,80 +1,50 @@
 'use client';
 import React, { useEffect, useState } from "react";
-import styles from "./styles.module.css";
-import { NextPage } from "next";
+import { styled } from '@mui/system';
+import { Grid, Typography } from '@mui/material';
+
+const Container = styled('div')({
+});
 
 const AboutUs = () => {
   const [teamData, setTeamData] = useState([]);
-  const [topData, setTopData] = useState({
-    TopTitle: "",
-    TopDesc: "",
-    TopImage: "",
-    CardTitle1: "",
-    CardDesc1: "",
-    CardTitle2: "",
-    CardDesc2: "",
-    CardTitle3: "",
-    CardDesc3: "",
-    HeadMsgTitle: "",
-    HeadMsgDesc: "",
-    HeadName: "",
-    HeadImage: "",
-    // TeamName: "",
-    // Post: "",
-    // TeamImage: ""
+  const [aboutUsData, setAboutUsData] = useState({
+    chairman_img_url: "",
+    chairman_msg: "",
+    chairman_name: '',
+    description: "",
+    mission: "",
+    goal: "",
+    title: "",
+    vision: "",
+    img_url: ""
   });
-
-  // const OurTeam = () => {
-  //   const [teamData, setTeamData] = useState([]);
-
-  //   useEffect(() => {
-  //     fetch("http://localhost:1337/api/our-teams?populate=*")
-  //         .then((response) => response.json())
-  //         .then((data) => {
-  //             // Assuming data is an array of team items
-  //             setTeamData(data.data);
-  //         })
-  //         .catch((error) => {
-  //             console.error("Error fetching data:", error);
-  //         });
-  // }, []);
-
+  const [hoveredMember, setHoveredMember] = useState(null);
   useEffect(() => {
     Promise.all([
-      fetch("http://localhost:1337/api/about-uses?populate=*"),
-      fetch("http://localhost:1337/api/our-teams?populate=*")
-    ])
-      .then((responses) => Promise.all(responses.map((res) => res.json())))
+    fetch("http://localhost:1337/api/about-us?populate=*"),
+    fetch("http://localhost:1337/api/teams?populate=*")
+  ])
+      .then((responses) => Promise.all(responses.map((res => res.json())))
       .then(([aboutUsData, teamsData]) => {
-        const topItem = aboutUsData.data[0];
-        const imageUrl = topItem.attributes.TopImage.data.attributes.url;
-        const headimage = topItem.attributes.HeadImage.data.attributes.url;
-        const teamimage = teamsData.data[3].attributes.TeamImage.data.attributes.url;
-        console.log(teamsData.data)
-        setTopData({
-          TopTitle: topItem.attributes.TopTitle,
-          TopDesc: topItem.attributes.TopDesc,
-          TopImage: imageUrl,
-          CardTitle1: topItem.attributes.CardTitle1,
-          CardTitle2: topItem.attributes.CardTitle2,
-          CardTitle3: topItem.attributes.CardTitle3,
-          CardDesc1: topItem.attributes.CardDesc1,
-          CardDesc2: topItem.attributes.CardDesc2,
-          CardDesc3: topItem.attributes.CardDesc3,
-          HeadMsgTitle: topItem.attributes.HeadMsgTitle,
-          HeadMsgDesc: topItem.attributes.HeadMsgDesc,
-          HeadName: topItem.attributes.HeadName,
-          HeadImage: headimage,
-          // TeamName: teamsData.data[3].attributes.TeamName,
-          // Post: teamsData.data[3].attributes.Post,
-          // TeamImage: teamimage
+
+        setAboutUsData({
+          chairman_img_url: 'http://localhost:1337' + aboutUsData.data.attributes.chairman_img_url.data.attributes.url,
+          chairman_msg: aboutUsData.data.attributes.chairman_msg,
+          chairman_name: aboutUsData.data.attributes.chairman_name,
+          description: aboutUsData.data.attributes.description,
+          mission: aboutUsData.data.attributes.mission,
+          goal: aboutUsData.data.attributes.goal,
+          title: aboutUsData.data.attributes.title,
+          vision: aboutUsData.data.attributes.vision,
+          img_url: 'http://localhost:1337' + aboutUsData.data.attributes.img_url.data.attributes.url
         });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-      });
+      }));
 
-      fetch("http://localhost:1337/api/our-teams?populate=*")
+      fetch("http://localhost:1337/api/teams?populate=*&pagination[page]=1&pagination[pageSize]=8")
       .then((response) => response.json())
       .then((data) => {
           // Assuming data is an array of team items
@@ -85,162 +55,102 @@ const AboutUs = () => {
       });
   }, []);
 
-//   return (
-//     <>
-//         <div className={styles.teamcontainer}>
-//             {teamData.map((teamItem, index) => (
-//                 <div className={styles.teamfieldcontainer} key={index}>
-//                     <div className={styles.teamfield}>
-//                         <div className={styles.teamimage}>
-//                             <img src={`http://localhost:1337${teamItem.attributes.TeamImage.data.attributes.url}`} alt="image" />
-//                         </div>
-//                         <div className={styles.textname}>{teamItem.attributes.TeamName}</div>
-//                         <div className={styles.textpost}>{teamItem.attributes.Post}</div>
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-//     </>
-// );
-// };
   return (
-    <>
-    <TopMessage topData={topData} />
+<Container >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <img src={aboutUsData.img_url} alt="image" style={{ width: '100%', height: 'auto' }} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={8} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        
+          <Typography variant="h4" sx={{ marginBottom: '10px', color:'black', font:'24px', weight:'bold',}}>
+            {aboutUsData.title}
+          </Typography> 
+          
+          <Typography variant="body1" sx={{color:'black'}}>
+            {aboutUsData.description}
+          </Typography>
+        </Grid>
+      </Grid>
 
-      <div className={styles.gridcontainer}>
-        <div className={styles.textfieldcontainer}>
-          <div className={styles.textfield}>
-            <div className={styles.textcontent}>{topData.CardTitle1}</div>
-            <div className={styles.textcontent}>
-             {topData.CardDesc1}
-            </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <div>
+            <Typography variant="h6" sx={{color:'black'}}>Our Mission</Typography>
+            <Typography variant="body1" sx={{color:'black'}}>{aboutUsData.mission}</Typography>
           </div>
-        </div>
-        <div className={styles.textfieldcontainer}>
-          <div className={styles.textfield}>
-            <div className={styles.textcontent}>{topData.CardTitle2}</div>
-            <div className={styles.textcontent}>
-            {topData.CardDesc2}
-            </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div>
+            <Typography variant="h6" sx={{color:'black'}}>Our Vision</Typography>
+            <Typography variant="body1" sx={{color:'black'}}>{aboutUsData.vision}</Typography>
           </div>
-        </div>
-        <div className={styles.textfieldcontainer}>
-          <div className={styles.textfield}>
-            <div className={styles.textcontent}>{topData.CardTitle3}</div>
-            <div className={styles.textcontent}>
-            {topData.CardDesc3}
-            </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div>
+            <Typography variant="h6" sx={{color:'black'}}>Our Goal</Typography>
+            <Typography variant="body1" sx={{color:'black'}}>{aboutUsData.goal}</Typography>
           </div>
-        </div>
-      </div>
-      <ChairmanMsg topData={topData} />
-      {/* <TeamField topData={topData}/> */}
-      <div className={styles.teamcontainer}>
-                {teamData.map((teamItem, index) => (
-                    <div className={styles.teamfieldcontainer} key={index}>
-                        <div className={styles.teamfield}>
-                            <div className={styles.teamimage}>
-                                <img src={`http://localhost:1337${teamItem.attributes.TeamImage.data.attributes.url}`} alt="image" />
-                            </div>
-                            <div className={styles.textname}>{teamItem.attributes.TeamName}</div>
-                            <div className={styles.textpost}>{teamItem.attributes.Post}</div>
-                        </div>
-                    </div>
+        </Grid>
+      </Grid>
+
+         <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} md={8} sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Typography variant="h4" sx={{ marginBottom: '10px', color: 'black', fontWeight: 'bold' }}>
+          Message from Our Head
+        </Typography>
+        <Typography variant="body1" sx={{ color: 'black' }}>
+          {aboutUsData.chairman_msg}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ color: 'black' }}>
+          {aboutUsData.chairman_name}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <div style={{ background: '#2161CD', width: '100%', height: '100%', position: 'absolute', zIndex: -1 }} />
+        <img src={aboutUsData.chairman_img_url} alt="image" style={{ width: '100%', height: 'auto' }} />
+      </Grid>
+    </Grid>
+
+      <Typography variant="h2">
+        Our Team
+      </Typography>
+
+      <Grid container spacing={2}>
+        {teamData.map((teamItem, index) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            key={index}
+            onMouseEnter={() => setHoveredMember(index)}
+            onMouseLeave={() => setHoveredMember(null)}
+          >
+            <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden', margin: '0 auto' }}>
+              <img src={`http://localhost:1337${teamItem.attributes.img_url.data.attributes.url}`} alt="image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            </div>
+            <Typography variant="h5" sx={{ color: 'black' }}>
+              {teamItem.attributes.name}
+            </Typography>
+            <Typography variant="subtitle1" sx={{ color: 'blue' }}>
+              {teamItem.attributes.designation}
+            </Typography>
+            {hoveredMember === index && (
+              <ul style={{color:'black'}}>
+               <div style={{color:'black'}}>Roles</div> 
+                {teamItem.attributes.role.map((goalItem) => (
+                  <li key={goalItem.id}>{goalItem.name}</li>
                 ))}
-            </div>
-    </>
+              </ul>
+            )}
+          </Grid>
+        ))}
+      </Grid>
+
+
+    </Container>
   );
 };
-
-// const TeamField: NextPage<Props> = (props) => {
-//   const { topData } = props;
-
-//   if (!topData || !topData.TeamName) {
-//     return null; // Render nothing if topData is null, undefined, or does not contain TeamName
-//   }
-
-//   let img_url = `http://localhost:1337${topData.TeamImage}`;
-//   return (
-//     <>
-//       <div className={styles.App}>Our Team</div>
-//       <div className={styles.teamcontainer}>
-//         <div className={styles.teamfieldcontainer}>
-//           <div className={styles.teamfield}>
-//             <div className={styles.teamimage}>
-//               <img src={img_url} alt={`Team Member ${topData.TeamName}`} />
-//             </div>
-//             <div className={styles.textname}>{topData.TeamName}</div>
-//             <div className={styles.textpost}>{topData.Post}</div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-
-
-interface Props{
- topData: {
-  TopTitle: string,
-  TopDesc: string,
-  TopImage: string,
-  HeadMsgTitle: string,
-  HeadMsgDesc: string,
-  HeadName:string,
-  HeadImage:string,
-  // TeamName: string,
-  // Post: string,
-  // TeamImage: string,
- }
-}
-
-const TopMessage: NextPage<Props> = (props) => {
-  const { topData } = props;
-  // console.log(topData.TopImage);
-  let img_url = `http://localhost:1337${topData.TopImage}`
-  return (
-    <div>
-      <div className={styles.wholecontainer}>
-        <div className={styles.imagecont}>
-        <img src={img_url} alt="image" />
-        </div>
-        <div className={styles.rightfield}>
-          <div className={styles.toptitle}>{topData.TopTitle}</div>
-          <div className={styles.topdesc}>{topData.TopDesc}</div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const ChairmanMsg: NextPage<Props> = (props) => {
-  const { topData } = props;
-  let img_url = `http://localhost:1337${topData.HeadImage}`
-  return(
-    <div>
-      <div className={styles.completecontainer}>
-      <div className={styles.msgcontainer}>
-      
-      
-      <div className={styles.msgtitle}>
-        {topData.HeadMsgTitle}
-      </div>
-      <div className={styles.msgdesc}>
-    {topData.HeadMsgDesc}
-      </div>
-      <div className={styles.chairmanname}>
-      {topData.HeadName}
-      </div>
-      </div>
-      <div className={styles.chairmanimage}>
-        <img src={img_url} alt="image" />
-      </div>
-    </div>
-    </div>
-  );
-  
-  };
-
 
 export default AboutUs;
--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,42 +1,60 @@
 'use client';
-import React, { useEffect, useState, FC } from "react";
-import styles from "./styles.module.css";
-import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import { Grid, Typography } from "@mui/material";
 
 const OurTeam = () => {
-    const [teamData, setTeamData] = useState([]);
+  const [teamData, setTeamData] = useState([]);
+  const [hoveredMember, setHoveredMember] = useState(null);
 
-    useEffect(() => {
-        fetch("http://localhost:1337/api/our-teams?populate=*")
-            .then((response) => response.json())
-            .then((data) => {
-                // Assuming data is an array of team items
-                setTeamData(data.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:1337/api/teams?populate=*")
+      .then((response) => response.json())
+      .then((data) => {
+        // Assuming data is an array of team items
+        setTeamData(data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-    return (
-        <>
-            <div className={styles.teamcontainer}>
-                {teamData.map((teamItem, index) => (
-                    <div className={styles.teamfieldcontainer} key={index}>
-                        <div className={styles.teamfield}>
-                            <div className={styles.teamimage}>
-                                <img src={`http://localhost:1337${teamItem.attributes.TeamImage.data.attributes.url}`} alt="image" />
-                            </div>
-                            <div className={styles.textname}>{teamItem.attributes.TeamName}</div>
-                            <div className={styles.textpost}>{teamItem.attributes.Post}</div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <Grid container spacing={2}>
+      {teamData.map((teamItem, index) => (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          key={index}
+          onMouseEnter={() => setHoveredMember(index)}
+          onMouseLeave={() => setHoveredMember(null)}
+        >
+          <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden', margin: '0 auto' }}>
+            <img src={`http://localhost:1337${teamItem.attributes.img_url.data.attributes.url}`} alt="image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          </div>
+          <Typography variant="h5" style={{ color: 'black', textAlign: 'center' }}>
+            {teamItem.attributes.name}
+          </Typography>
+          <Typography variant="subtitle1" style={{ color: 'blue', textAlign: 'center' }}>
+            {teamItem.attributes.designation}
+          </Typography>
+          {hoveredMember === index && (
+            <ul style={{ color: 'black', textAlign: 'center', listStyle: 'none', padding: 0 }}>
+              <li style={{ fontWeight: 'bold' }}>Roles:</li>
+              {teamItem.attributes.role.map((goalItem) => (
+                <li key={goalItem.id}>{goalItem.name}</li>
+              ))}
+            </ul>
+          )}
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default OurTeam;
+
+
 
 // pagination = http://localhost:1337/api/our-teams?populate=TeamImage&pagination[page]=1&pagination[pageSize]=2
