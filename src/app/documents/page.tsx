013883b1fd9a5916a5b061f2f97a1249c2b2cef3
Dipeshlohani <dipeshlohanii@gmail.com>
2023-11-03 13:13:30 +0545

    document UI added

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,5 +1,105 @@
-import { Box } from '@mui/material';
-import Image from 'next/image'
+'use client';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, IconButton, Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+interface Document {
+  id: number;
+  name: string;
+  file_url: {
+    data: {
+      url: string;
+    };
+  };
+}
+interface DocumentCategory {
+  id: number;
+  attributes: {
+    name: string;
+    document: Document[];
+  };
+  showBoxes?: boolean;
+}
+const DocumentPage = () => {
+  const [documentCategories, setDocumentCategories] = useState<DocumentCategory[]>([]);
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    fetch(`http://localhost:1337/api/document-categories?populate[0]=document&populate[1]=document.file_url`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDocumentCategories(data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+  const handleAddClick = (index: number) => {
+    const updatedCategories = [...documentCategories];
+    updatedCategories[index].showBoxes = !updatedCategories[index].showBoxes;
+    setDocumentCategories(updatedCategories);
+  };
+  return (
+    <div>
+      {documentCategories.map((category, index) => (
+        <div key={category.id}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              height: '50px',
+              width: '700px',
+              backgroundColor: 'white',
+              border: '1px solid #BCBABA',
+              padding: '0 20px',
+              borderRadius: '8px',
+              marginBottom: '10px',
+            }}
+          >
+            <Typography variant="body1" sx={{ color: 'black' }}>{category.attributes.name}</Typography>
+            <IconButton onClick={() => handleAddClick(index)}>
+              {category.showBoxes ? <RemoveIcon /> : <AddIcon />}
+            </IconButton>
+          </Box>
+          {category.showBoxes && (
+            <div>
+              {category.attributes.document.map((document) => (
+                <Box
+                  key={document.id}
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    height: '50px',
+                    width: '670px',
+                    backgroundColor: 'white',
+                    border: '1px solid #BCBABA',
+                    padding: '0 20px',
+                    margin: '10px 0',
+                    borderRadius: '8px',
+                    marginBottom: '10px',
+                  }}
+                >
+                  <Typography variant="body1" sx={{ color: 'black', marginRight: '20px' }}>{document.name}</Typography>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Button variant="outlined" sx={{ marginRight: '20px', borderColor: 'white' }}>
+                      View
+                    </Button>
+                    <div style={{ height: '22px', borderRight: '1px solid #2161CD', margin: '0 20px' }}></div>
+                    <Button variant="outlined" sx={{ borderColor: 'white' }}>
+                      Download
+                    </Button>
+                  </div>
+                </Box>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default DocumentPage;
 
 async function getData() {
   var options = {
@@ -18,37 +118,4 @@ async function getData() {
     throw new Error('Failed to fetch data')
   }
   return res.json()
-}
-
-export default async function Document() {
-  let { data } = await getData();
-  return (
-    <div>
-      <ul>
-        {
-          data.map(elem => (
-            <>
-              <Box
-                sx={{
-                  paddingHorizontal: 8,
-                  paddingVertical: 6,
-                  borderRadius: 4,
-                  backgroundColor: 'oldlace',
-                  alignSelf: 'flex-start',
-                  marginHorizontal: '1%',
-                  marginBottom: 6,
-                  minWidth: '48%',
-                  textAlign: 'left',
-                }}
-              >{elem.attributes.name}
-                {elem.attributes.document.map(doc => (
-                  <h1>{doc.name}</h1>
-                ))}
-              </Box>
-            </>
-          ))
-        }
-      </ul>
-    </div >
-  );
 }
