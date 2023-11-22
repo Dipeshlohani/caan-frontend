9621cd9961fed32a38912ef551bdc2a712fc9294
Shikhar <49984957+shikhardahal@users.noreply.github.com>
2023-11-22 10:02:37 +0545

    Search Page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,6 +6,8 @@ import Layout from '@/components/layout';
 import CallToAction from '@/components/cta';
 import SearchPageStyles from '@/components/search/search.module.css';
 import Pagination from '@mui/material/Pagination';
+import { InputAdornment } from '@mui/material';
+
 
 const SearchContainer = styled(Box)(SearchPageStyles.searchContainer);
 
@@ -33,33 +35,45 @@ const SearchPage = () => {
             marginBottom: '10px',
           }}
         >
-          <TextField
-            id="search"
-            label="Search Now..."
-            variant="outlined"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            sx={{ marginBottom: 2, width: '50%' }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSearch}
-            sx={{ width: '80px', height: '40px', borderRadius: '0', marginLeft: '10px' }}
-          >
-            Search Now
-          </Button>
-        </Box>
+           <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginBottom: '20px',
+            }}
+          ></Box>
+         <TextField
+              id="search"
+              variant="outlined"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              sx={{ width: '50%', marginRight: '10px' }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSearch}
+              sx={{ width: '80px', height: '40px', borderRadius: '0' }}
+            >
+              Search Now
+            </Button>
+          </Box>
+
 
         {/* Text above search results */}
         <Typography
           sx={{
-            marginTop: 1,
+            marginTop: -10,
             width: '476px',
             height: '29px',
             fontSize: '24px',
-            marginRight: '20px',
+            marginRight: '30px',
             textAlign: 'center',
+            textDecoration: 'underline',
+            marginBottom: '30px',
+            
           }}
         >
   
@@ -75,15 +89,16 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '10px', // Adjust as needed
         }}>
           Civil Airlines is the highway of the best thing in Nepal in 2023.
         </Typography>
 
         {/* Image 1 */}
-        <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
+        <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '5px', marginBottom: '10px' }}>
           <img
-            src="/images/your-image-filename.jpg" // Replace with the actual image path
+            src="rectangle.png" // Replace with the actual image path
             alt="Civil Airlines Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -98,7 +113,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px', // Adjust as needed
         }}>
           The existence of independent external audit, and the accompanying potential for scrutiny improves performance.
         </Typography>
@@ -106,7 +122,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src="Rectangle 171.png" // Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -119,6 +135,7 @@ const SearchPage = () => {
           letterSpacing: '0em',
           textAlign: 'center',
           marginTop: '10px',
+          marginBottom: '30px', 
            // Adjust as needed
         }}>
           Pellentesque eget fringilla quam. In sit amet auctor dui, at dictum quam. Cras nisl ligula, dapibus eu lorem vitae, porttitor tincidunt eros.
@@ -127,7 +144,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
         <img
-  src="/images/rectangle.png"
+  src="Rectangle 172.png"
   alt="Rectangle Image"
   style={{ maxWidth: '100px', maxHeight: '100px' }}
 />
@@ -140,7 +157,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px',  // Adjust as needed
         }}>
           
           Quisque gravida augue in lacus eleifend, id bibendum enim viverra. Ut vel pharetra arcu, id consectetur quam
@@ -150,7 +168,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src= "Rectangle 173.png"// Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -162,7 +180,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px',  // Adjust as needed
         }}>
           Morbi vitae libero non libero scelerisque sollicitudin vel eu eros. Pellentesque lobortis urna sit amet purus laoreet pellentesque
         </Typography>
@@ -170,7 +189,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src="Rectangle 174.png" // Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -182,7 +201,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px',  // Adjust as needed
         }}>
           Civil Airlines is the highway of the best thing in Nepal in 2023
 
@@ -191,7 +211,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src="Rectangle 175.png" // Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -203,7 +223,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px',  // Adjust as needed
         }}>
           Quisque pretium, est vitae tempor pharetra, ex mauris aliquet enim, et lacinia enim nisl a urna.
 
@@ -212,7 +233,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src="Rectangle 176.png" // Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
@@ -224,7 +245,8 @@ const SearchPage = () => {
           lineHeight: '25px',
           letterSpacing: '0em',
           textAlign: 'center',
-          marginTop: '10px', // Adjust as needed
+          marginTop: '10px',
+          marginBottom: '30px',  // Adjust as needed
         }}>
           Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos
         </Typography>
@@ -232,7 +254,7 @@ const SearchPage = () => {
         {/* Image 2 */}
         <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr', alignItems: 'center', gap: '10px', marginBottom: '10px' }}>
           <img
-            src="/images/your-audit-image.jpg" // Replace with the actual image path
+            src="Rectangle 177.png" // Replace with the actual image path
             alt="Audit Image"
             style={{ maxWidth: '100px', maxHeight: '100px' }}
           />
