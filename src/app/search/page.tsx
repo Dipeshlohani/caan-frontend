949b4068df5b80644ed871d6423e3c03b18ba92c
Dipeshlohani <dipeshlohanii@gmail.com>
2023-12-01 14:06:36 +0545

    updated search page design

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,8 +5,8 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Layout from "@/components/layout";
 import CallToAction from '@/components/cta';
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
-import { InstantSearch, Hits, Stats, SearchBox } from 'react-instantsearch';
-import { useInstantSearch, useSearchBox } from 'react-instantsearch';
+import { InstantSearch } from 'react-instantsearch';
+import { useHits } from 'react-instantsearch';
 
 import SearchBar from "./SearchBar"
 
@@ -15,7 +15,8 @@ import SearchIcon from '@mui/icons-material/Search';
 const searchClient = instantMeiliSearch(
   'http://localhost:7700/', // Host
   'SDj-iFrEiDstQrijYQoHdyk2aKxT8rpvONyME24a-kk', // API key
-  {}
+  {
+  }
 )
 
 const Search = () => {
@@ -42,30 +43,14 @@ const Search = () => {
       <Container>
         <InstantSearch indexName="activity" searchClient={searchClient}>
           {/* Search Bar */}
-          {/* <SearchBox /> */}
           <SearchBar />
-          {/* <Box display="flex" alignItems="center" justifyContent="space-between" mt={5} mb={3}>
-            <TextField
-              label="Search now"
-              variant="outlined"
-              placeholder="Search now"
-              fullWidth
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              InputProps={{ endAdornment: <SearchIcon color="primary" onClick={handleSearch} /> }}
-            />
-            <Button variant="contained" color="primary" sx={{ marginLeft: 2 }} onClick={handleSearch}>
-              Search Now
-            </Button>
-          </Box> */}
           {/* Search Results Header */}
           <Typography variant="h5" gutterBottom>
             Showing search results for - Civil Airlines section
           </Typography>
           {/* Search Results */}
           <Paper elevation={0} sx={{ p: 2 }}>
-            <Stats />
-            <Hits hitComponent={Hit} />
+            <CustomHits />
           </Paper>
 
           {/* Dotted Divider Line */}
@@ -77,20 +62,24 @@ const Search = () => {
   );
 };
 
-function Hit(props) {
-  console.log(props);
+function CustomHits(props) {
+  const { hits, sendEvent } = useHits(props);
   return (
-    <div key={props.hit.id}>
-      <Box display="flex" alignItems="center" mb={2}>
-        <img src={'http://localhost:1337' + props.hit.img_url.url} alt="Result 1" style={{ width: 100, height: 100, marginRight: 2 }} />
-        <Typography variant="body1">
-          {props.hit.description.substr(0, 150)}
-        </Typography>
-      </Box>
+    <ol style={{ textDecoration: 'none' }}>
+      {hits.map((hit) => (
+        <>
+          <Box display="flex" alignItems="center" mb={2}>
+            <img src={'http://localhost:1337' + hit?.img_url?.url} alt="Result 1" style={{ width: 100, height: 90, marginRight: 2 }} />
+            <Typography variant="body1" sx={{ p: 3 }}>
+              {hit.description?.substr(0, 150)}
+            </Typography>
+            {/* Divider Line */}
+          </Box>
+          <Divider sx={{ my: 2 }} />
+        </>
 
-      {/* Divider Line */}
-      <Divider sx={{ my: 2 }} />
-    </div>
+      ))}
+    </ol>
   );
 }
 
